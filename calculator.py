bd4254dfeee507c826ec916f1a3b58a5d65bff6e
aieckxis <aleckxiskate@gmail.com>
2023-05-21 16:41:45 +0800

    asked the user to enter 2 numbers

--- calculator.py
+++ calculator.py
@@ -12,11 +12,19 @@ class MathOpeations:
     # Ask the user for the operation to perform
     def get_operation(self):
         while True:
-            self.operation = input("Please choose a math operation (+, -, *, /)")
+            self.operation = input("Please choose a math operation (+, -, *, /): ")
             if self.operation in ("+", "-", "*", "/"):
                 break
             else:
                 print("Error: Invalid operation.")
 # Ask the user for two numbers
+    def get_numbers(self):
+        while True:
+            try:
+                self.num1 = float(input("Enter the first number: "))
+                self.num2 = float(input("Enter the second number: "))
+                break
+            except ValueError:
+                print("Error: Invalid input. Please enter a number.")
 # Calculate the result based on the chosen operation
 # Print the result
