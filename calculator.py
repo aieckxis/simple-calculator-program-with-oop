0df3ba9344a65cbc130aa3b88bd0498f3ef69b56
aieckxis <aleckxiskate@gmail.com>
2023-05-21 16:26:55 +0800

    asked the user to choose math operation

--- calculator.py
+++ calculator.py
@@ -2,7 +2,21 @@
 # BSCpE 1-4
 
 # Define Math Operations
-# Ask the user for the operation to perform
+class MathOpeations:
+    def __init__(self):
+        self.operation = ""
+        self.num1 = 0
+        self.num2 = 0
+        self.result = 0
+
+    # Ask the user for the operation to perform
+    def get_operation(self):
+        while True:
+            self.operation = input("Please choose a math operation (+, -, *, /)")
+            if self.operation in ("+", "-", "*", "/"):
+                break
+            else:
+                print("Error: Invalid operation.")
 # Ask the user for two numbers
 # Calculate the result based on the chosen operation
 # Print the result
