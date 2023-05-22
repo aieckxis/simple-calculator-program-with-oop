ecaad3ed7543ed03bc6a2d5d65b702a8294e9ceb
aieckxis <aleckxiskate@gmail.com>
2023-05-22 16:45:48 +0800

    performed the math operation before asking the user to try again but program still working when user entered n

--- import.py
+++ import.py
@@ -11,10 +11,10 @@ def main():
         math.calculate_result()
         math.print_result()
 
-    # Ask the user if they want to try again
-    try_again = input("Do you want to try again? (y/n): ")
-    if try_again == "n":
-        print("Okay, thank you!")
+        # Ask the user if they want to try again
+        try_again = input("Do you want to try again? (y/n): ").lower()
+        if try_again == "n":
+            print("Okay, thank you!")
 
 # Call the main function to start the program
 if __name__ == "__main__":
