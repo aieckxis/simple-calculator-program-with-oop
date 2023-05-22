6c59f2878a32dcd5dc60db6623f64513c2e5e06a
aieckxis <aleckxiskate@gmail.com>
2023-05-22 16:20:37 +0800

    input try_again, asked the user to try again without performing the math operation first

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 # Import the calculator.py
 from calculator import MathOperations
+
 # Create a MathOperations object
 def main():
     math = MathOperations()
@@ -9,7 +10,12 @@ def main():
         math.get_numbers()
         math.calculate_result()
         math.print_result()
+
 # Ask the user if they want to try again
+try_again = input("Do you want to try again? (y/n): ")
+if try_again == "n":
+    print("Okay, thank you!")
+
 # Call the main function to start the program
 if __name__ == "__main__":
     main()
